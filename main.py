eec81a7593fcdd0361cb67677967ac3d3f16fa42
s-gundorov <46057845+s-gundorov@users.noreply.github.com>
2024-03-23 16:39:50 +0300

    add target

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -9,6 +10,13 @@ screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
 pygame.display.set_caption('ИГРА ТИР')
 icon1 = pygame.image.load("_hand.ico")
 pygame.display.set_icon(icon1)
+
+target = pygame.image.load("target.ico")
+target_width = 50
+target_height = 50
+target_x = SCREEN_WIDTH
+target_y = SCREEN_HEIGHT
+
 
 running = True
 
