7f889df44133e5869dfd5d490a6ad42a9931b6af
s-gundorov <46057845+s-gundorov@users.noreply.github.com>
2024-03-23 14:24:46 +0300

    Установка констант

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
+pygame.display.set_caption('ИГРА ТИР')
+
 running = True
 
-while running
+while running:
     pass
 
 pygame.quit()
