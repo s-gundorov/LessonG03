927516bcf1c84c6e5bd32e2d5749216ed463388f
s-gundorov <46057845+s-gundorov@users.noreply.github.com>
2024-03-23 15:14:27 +0300

    Добавление иконки

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode( (SCREEN_WIDTH, SCREEN_HEIGHT) )
 pygame.display.set_caption('ИГРА ТИР')
+pygame.image.load("_hand.ico")
+pygame.display.set_icon(icon1)
 
 running = True
 
